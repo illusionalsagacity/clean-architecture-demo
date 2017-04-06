3d9bbad72a848e9fb055487e8c0262fa528cccf1
illusionalsagacity <illusionalsagacity@gmail.com>
2017-04-06 17:55:15 -0400

    fix: improper usecase

--- src/Todo/containers/TodoFormContainer.js
+++ src/Todo/containers/TodoFormContainer.js
@@ -2,19 +2,10 @@
 
 import { connect } from "react-redux";
 import TodoFormComponent from "../components/TodoFormComponent";
-
-import usecases from "../usecases";
-import * as services from "../services";
-const { createTodoService, createValidationService } = services;
-const { createAddTodoUsecase } = usecases;
+import { addTodoUsecase } from "store";
 
 const makeMapDispatchToProps = () => {
   const mapDispatchToProps = (dispatch, ownProps) => {
-    const addTodoUsecase = createAddTodoUsecase({
-      TodoService: createTodoService(dispatch),
-      ValidationService: createValidationService(dispatch),
-    });
-
     return {
       addTodo: (name: string, description: string) => addTodoUsecase(ownProps.userID, name, description),
     };
