796c4f9256ab778f986b3567c0e39da8bb1610ff
illusionalsagacity <illusionalsagacity@gmail.com>
2017-04-17 09:13:38 -0400

    feat: example of usecase test

---
+++ test/Todo/usecases.test.js
@@ -0,0 +1,32 @@
+import test from "ava";
+import sinon from "sinon";
+import usecases from "Todo/usecases";
+const { AddTodoUsecase } = usecases;
+
+
+test("AddTodoUsecase", t => {
+  const services = {
+    ValidationService: {
+      validateTodo: sinon.spy(() => Promise.resolve(true)),
+    },
+    TodoService: {
+      createTodo: sinon.spy((description, name, creatorID, date) => ({ id: "foo", description, name, creatorID, date })),
+    },
+    ErrorService: {
+      reportError: sinon.spy(err => console.log(err)),
+    },
+  };
+
+  t.truthy(AddTodoUsecase, "AddTodoUsecase is defined");
+  const addTodoUsecase = AddTodoUsecase(services);
+  t.truthy(addTodoUsecase, "the curried usecase is defined");
+
+  // Here we're just checking to make sure the services provided were called. We don't care about if the services *did*
+  // anything because they'll have their own tests
+  addTodoUsecase("bar", "name", "description", "2017-01-01").then(data => {
+    t.true(services.ValidationService.validateTodo.calledOnce);
+    t.false(services.ErrorService.reportError.calledOnce);
+    t.is(services.ErrorService.reportError.called, 0);
+    t.true(services.TodoService.createTodo.calledOnce);
+  });
+});
