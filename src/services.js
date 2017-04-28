075cdfdbbaf473ba1a6ecac45a2228c6fad49d66
illusionalsagacity <illusionalsagacity@gmail.com>
2017-04-27 23:56:30 -0400

    chore: remove debugger

--- src/services.js
+++ src/services.js
@@ -3,7 +3,6 @@ import { dispatch, getState } from "./store";
 
 // This creates a function that passes in dispatch and getState to all the uninitialized services.
 //
-debugger;
 const initializeServices = createInitializeServices(dispatch, getState);
 
 // You could use this to initialize all the services in the application, then pull out the ones you need for each usecase.
